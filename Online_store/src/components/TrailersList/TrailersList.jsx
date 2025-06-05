161e3a760805da97ae1d2a29295e24278e1c2886
SergeRomanenko13 <serhiiromanenko13@gmail.com>
2025-06-05 11:41:13 +0300

    Add basic files to Project

--- Online_store/src/components/TrailersList/TrailersList.jsx
+++ Online_store/src/components/TrailersList/TrailersList.jsx
@@ -1,7 +1,7 @@
 import services from "./../../services/mockapi";
 import { useState, useEffect } from "react";
 import styles from "./TrailersList.module.scss";
-
+import data from "./../../data";
 export default function TrailersList() {
 	const [trailers, setTrailers] = useState([]);
 	console.log(trailers);
@@ -11,11 +11,7 @@ export default function TrailersList() {
 			// const response = await service.get("trailers");
 			// setTrailers(response.data.trailers);
 			// console.log(response.data.trailers);
-			setTrailers([
-				{ brand: "Kia", id: 1, model: "S", price: 150 },
-				{ brand: "Renault", id: 2, model: "L", price: 200 },
-				{ brand: "Ford", id: 3, model: "M", price: 250 },
-			]);
+			setTrailers(data);
 		} catch (error) {
 			console.log(error);
 		}
@@ -30,7 +26,7 @@ export default function TrailersList() {
 			{trailers.length > 0
 				? trailers.map((trailer) => {
 						return (
-							<div className={styles.trailer__card} key={trailer.id}>
+							<div className={styles.trailer__card} key={trailer._id}>
 								<p>{trailer.brand}</p>
 								<p>{trailer.model}</p>
 								<p>{trailer.price}</p>
